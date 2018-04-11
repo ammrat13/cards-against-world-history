ad84227215700ca6e5ab8fba49b744d5de8c4ac6
ammrat13 <ammrat13@gmail.com>
2018-04-10 23:52:50 -0500

    Made management script

---
+++ man.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# For managing the server
+# Pass `up` to start and `down` to stop
+
+if [[ $1 == "up" ]]; then
+	# Check if we already have it up
+	wget -q --spider localhost
+	if [[ $? -eq 0 ]]; then
+		echo "Server is already up"
+	else
+		echo "Starting server"
+		cd backend
+		javac Server.java && sudo bash -c 'java Server > /dev/null &'
+		cd ..
+		echo "Started server"
+	fi
+elif [[ $1 == "down" ]]; then
+	echo "Stopping server"
+	sudo killall java
+	echo "Stopped server"
+fi
