a5ecc17b8b07adf8503da9945682d85b78953487
ammrat13 <ammrat13@gmail.com>
2018-04-12 12:25:45 -0500

    Modified management script

--- man.sh
+++ man.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # For managing the server
-# Pass `up` to start and `down` to stop
+# Pass `start` to start and `stop` to stop and `restart` to restart
 
 if [[ $1 == "" ]]; then
 	echo "No arguments present"
-elif [[ $1 == "up" ]]; then
+elif [[ $1 == "start" ]]; then
 	# Check if we already have it up
 	wget -q --spider localhost
 	if [[ $? -eq 0 ]]; then
@@ -17,10 +17,13 @@ elif [[ $1 == "up" ]]; then
 		cd ..
 		echo "Started server"
 	fi
-elif [[ $1 == "down" ]]; then
+elif [[ $1 == "stop" ]]; then
 	echo "Stopping server"
 	sudo killall java
 	echo "Stopped server"
+elif [[ $1 == "restart" ]]; then
+	./$0 stop
+	./$0 start
 else
 	echo "Illegal argument: $1"
 fi
