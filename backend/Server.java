93e3c9d27821147d0789a121b124d9db312236a4
ammrat13 <ammrat13@gmail.com>
2018-04-13 08:59:33 -0500

    Deleted logging

--- backend/Server.java
+++ backend/Server.java
@@ -106,8 +106,7 @@ public class Server {
 			// Every time we get a request, prune the games
 			for(String pin : games.keySet()){
 				games.get(pin).prune();
-				System.out.println(System.currentTimeMillis() - games.get(pin).gameTimeout);
-				if(System.currentTimeMillis() - games.get(pin).gameTimeout > 60*1000)
+				if(System.currentTimeMillis() - games.get(pin).gameTimeout >= 60*1000)
 					games.remove(pin);
 				else if(games.get(pin).cardCzar != -1 && games.get(pin).playerScores.size() == 0)
 					games.remove(pin);
